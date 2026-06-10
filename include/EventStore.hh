c20f3c67433668a8db3c7eb2f3aebc2a7f49bce0
Elliott Wright-Letterer <157049959+ElliottPWright@users.noreply.github.com>
2026-06-10 15:24:02 +0200

    Update EventStore.hh for reconstruction

--- include/EventStore.hh
+++ include/EventStore.hh
@@ -6,9 +6,31 @@
 class EventStore {
 public:
   void clear();
-  void addHit(const ParsedID& id, double edep, const G4ThreeVector& pos);
-  void addHit(const ParsedID&, double edep, const G4ThreeVector& posGlobal, const G4ThreeVector& posLocal);
-  // vectors filled per event
-  std::vector<double> edep, x_global, y_global, z_global, x_local, y_local, z_local;
-  std::vector<int> type, section, layer, vol, hcal, hpl_sublayer, hexant;
+
+  void addHit(const ParsedID& id,
+              double edep,
+              const G4ThreeVector& pos);
+
+  void addHit(const ParsedID&,
+              double edep,
+              const G4ThreeVector& posGlobal,
+              const G4ThreeVector& posLocal);
+
+  void addLayerCrossing(int hcal,
+                        int layer,
+                        const G4ThreeVector& pos);
+
+  std::vector<double> edep, x_global, y_global, z_global;
+  std::vector<double> x_local, y_local, z_local;
+
+  std::vector<int> type, section, layer, vol, hcal,
+                   hpl_sublayer, hexant;
+
+  // Truth track points
+  std::vector<int> crossing_layer;
+  std::vector<int> crossing_hcal;
+
+  std::vector<double> crossing_x;
+  std::vector<double> crossing_y;
+  std::vector<double> crossing_z;
 };
